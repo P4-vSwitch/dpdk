85b87581180208d153baf476ccaba411f7d31c8c
mshahbaz <mshahbaz@cs.princeton.edu>
2015-12-13 23:55:55 -0600

    applied patch
    kni: fix igb build with kernel 4.2

--- lib/librte_eal/linuxapp/kni/ethtool/igb/kcompat.h
+++ lib/librte_eal/linuxapp/kni/ethtool/igb/kcompat.h
@@ -3899,6 +3899,11 @@ skb_set_hash(struct sk_buff *skb, __u32 hash, __always_unused int type)
 
 #if ( LINUX_VERSION_CODE >= KERNEL_VERSION(4,1,0) )
 /* ndo_bridge_getlink adds new nlflags parameter */
-#define HAVE_NDO_BRIDGE_GETLINK_FILTER_MASK
+#define HAVE_NDO_BRIDGE_GETLINK_NLFLAGS
 #endif /* >= 4.1.0 */
+
+#if ( LINUX_VERSION_CODE >= KERNEL_VERSION(4,2,0) )
+/* ndo_bridge_getlink adds new filter_mask and vlan_fill parameters */
+#define HAVE_NDO_BRIDGE_GETLINK_FILTER_MASK_VLAN_FILL
+#endif /* >= 4.2.0 */
 #endif /* _KCOMPAT_H_ */
